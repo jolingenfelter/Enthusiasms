6c0f86c1e97c3c84cf78f3b23c66f22a646bbede
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2016-10-23 17:42:51 -0400

    working toolbar for webview navigation

--- EnthusiasmsApp/EnthusiasmsApp/GetWebContentViewController.swift
+++ EnthusiasmsApp/EnthusiasmsApp/GetWebContentViewController.swift
@@ -14,8 +14,10 @@ class GetWebContentViewController: UIViewController, WKNavigationDelegate, UITex
     var webView = WKWebView()
     let navBar = UINavigationBar()
     let urlTextField = UITextField()
-    let barView = UIView()
-    
+    let toolbar = UIToolbar()
+    var backButton = UIBarButtonItem()
+    var forwardButton = UIBarButtonItem()
+    var refreshButton = UIBarButtonItem()
     
     override func loadView() {
         super.loadView()
@@ -25,18 +27,15 @@ class GetWebContentViewController: UIViewController, WKNavigationDelegate, UITex
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // WebView Setup
-        let url = URL(string: "http://www.google.com")
-        let request = URLRequest(url: url!)
-        self.webView.load(request)
+        view.backgroundColor = UIColor.white
         
         // NavBar Setup
         navBar.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: 60)
         view.addSubview(navBar)
-        
-        // barView Setup
-        barView.backgroundColor = UIColor.white
+        let navItem = UINavigationItem()
+        let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelPressed))
+        navItem.rightBarButtonItem = cancelButton
+        navBar.items = [navItem]
         
         // TextField Setup
         urlTextField.keyboardType = .URL
@@ -54,6 +53,24 @@ class GetWebContentViewController: UIViewController, WKNavigationDelegate, UITex
         urlTextField.leftViewMode = .always
         urlTextField.autocapitalizationType = .none
         
+        // WebView Setup
+        webView.addObserver(self, forKeyPath: "loading", options: .new, context: nil)
+        let url = URL(string: "http://www.google.com")
+        let request = URLRequest(url: url!)
+        self.webView.load(request)
+        
+        // Toolbar Setup
+        view.addSubview(toolbar)
+        backButton = UIBarButtonItem(title: "<", style: .plain, target: self, action: #selector(backPressed))
+        forwardButton = UIBarButtonItem(title: ">", style: .plain, target: self, action: #selector(forwardPressed))
+        refreshButton = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(refreshPressed))
+        let fixedSpaceItem = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+        fixedSpaceItem.width = 10
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolbar.setItems([backButton, fixedSpaceItem, forwardButton, flexibleSpace, refreshButton], animated: true)
+        backButton.isEnabled = false
+        forwardButton.isEnabled = false
+        
     }
     
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
@@ -63,6 +80,23 @@ class GetWebContentViewController: UIViewController, WKNavigationDelegate, UITex
     override func viewDidLayoutSubviews() {
         textFieldConstraints()
         webViewConstraints()
+        toolbarConstraints()
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "loading" {
+            backButton.isEnabled = webView.canGoBack
+            forwardButton.isEnabled = webView.canGoForward
+        }
+    }
+    
+    deinit {
+        webView.removeObserver(self, forKeyPath: "loading", context: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
     // MARK: View Setup
@@ -86,7 +120,19 @@ class GetWebContentViewController: UIViewController, WKNavigationDelegate, UITex
         let leadingConstraint = webView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
         let trailingConstraint = webView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         let topConstraint = webView.topAnchor.constraint(equalTo: urlTextField.bottomAnchor, constant: 5)
-        let bottomConstraint = webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        let bottomConstraint = webView.bottomAnchor.constraint(equalTo: toolbar.topAnchor)
+        
+        NSLayoutConstraint.activate([leadingConstraint, trailingConstraint, topConstraint, bottomConstraint])
+    }
+    
+    func toolbarConstraints() {
+        
+        toolbar.translatesAutoresizingMaskIntoConstraints = false
+        
+        let leadingConstraint = toolbar.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        let trailingConstraint = toolbar.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        let topConstraint = toolbar.topAnchor.constraint(equalTo: webView.bottomAnchor)
+        let bottomConstraint = toolbar.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         
         NSLayoutConstraint.activate([leadingConstraint, trailingConstraint, topConstraint, bottomConstraint])
     }
@@ -107,10 +153,22 @@ class GetWebContentViewController: UIViewController, WKNavigationDelegate, UITex
         
         return false
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: BarButtonItem Actions
+    
+    func cancelPressed() {
+        self.dismiss(animated: true, completion: nil)
     }
-
+    
+    func backPressed() {
+        webView.goBack()
+    }
+    
+    func forwardPressed() {
+        webView.goForward()
+    }
+    
+    func refreshPressed() {
+        webView.reload()
+    }
 }
