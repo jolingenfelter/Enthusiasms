de61ad95126766b4cd7a05e37da8d2732556de4c
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2017-01-11 16:05:29 -0500

    Adjustment to last fix

--- EnthusiasmsApp/EnthusiasmsApp/FullScreenImageViewController.swift
+++ EnthusiasmsApp/EnthusiasmsApp/FullScreenImageViewController.swift
@@ -95,7 +95,7 @@ class FullScreenImageViewController: UIViewController, UIScrollViewDelegate {
         self.title = content?.title
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(donePressed))
         self.navigationItem.rightBarButtonItem = doneButton
-        if rewardTime >= 0 {
+        if rewardTime > 0 {
             addTimeButton.addTarget(self, action: #selector(addTimePressed), for: .touchUpInside)
             addTimeButton.setTitleColor(.black, for: .normal)
             let addTimeBarButton = UIBarButtonItem.init(customView: addTimeButton)
