08e026d858b3fc9d292daea1967500d2e71a4054
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2017-01-19 14:21:39 -0500

    Added bugPrinter

---
+++ EnthusiasmsApp/BugPrinter.swift
@@ -0,0 +1,17 @@
+//
+//  BugPrinter.swift
+//  EnthusiasmsApp
+//
+//  Created by Joanna Lingenfelter on 1/19/17.
+//  Copyright © 2017 JoLingenfelter. All rights reserved.
+//
+
+import Foundation
+
+
+func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
+    
+    #if DEBUG
+    Swift.print(items[0], separator: separator, terminator: terminator)
+    #endif
+}
