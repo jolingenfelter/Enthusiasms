abce893427ff65947c86621449959311cc235b64
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2017-05-31 17:23:41 -0400

    Refactor TimeDisplay in StudentCollectionVC

--- EnthusiasmsApp/EnthusiasmsApp/StudentCollectionViewController.swift
+++ EnthusiasmsApp/EnthusiasmsApp/StudentCollectionViewController.swift
@@ -18,22 +18,12 @@ class StudentCollectionViewController: TeacherCollectionViewController {
     let addTimeViewController = AddTimeViewController()
     let addTimePasswordCheck = AddTimePasswordCheckViewController()
     
-    var minutes: Int {
-        return rewardTime / 60
-    }
-    var seconds: Int {
-        return rewardTime % 60
-    }
-    
-    var timeDisplay: String {
-        if seconds == 0 {
-            return "\(minutes):00"
-        } else if seconds >= 1 && seconds < 10 {
-            return "\(minutes):0\(seconds)"
-        } else {
-            return "\(minutes):\(seconds)"
-        }
-    }
+    lazy var timeDisplay: TimeDisplay = {
+        
+        let timeDisplay = TimeDisplay(timeInSeconds: self.rewardTime)
+        return timeDisplay
+        
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -110,7 +100,8 @@ class StudentCollectionViewController: TeacherCollectionViewController {
     func updateTimer() {
         
         rewardTime -= 1
-        addTimeButton.setTitle(timeDisplay, for: .normal)
+        timeDisplay.totalTimeInSeconds = rewardTime
+        addTimeButton.setTitle(timeDisplay.display, for: .normal)
         let enterPasswordVC = EnterPasswordViewController()
         
         if rewardTime == 0 {
