0ab409c3e9e41e2beeddb2103518dfef655fbe00
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2017-06-07 17:24:49 -0400

    Refactor StudentListVC

--- EnthusiasmsApp/EnthusiasmsApp/StudentListViewController.swift
+++ EnthusiasmsApp/EnthusiasmsApp/StudentListViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 import CoreData
 
-class StudentListViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+class StudentListViewController: UITableViewController {
     
     var settingsButton = UIBarButtonItem()
     var addButton = UIBarButtonItem()
     var allContentButton = UIBarButtonItem()
-    let instructionsLabel = UILabel()
+    
+    lazy var instructionsLabel: UILabel = {
+        
+        let label = UILabel()
+        label.text = "Tap '+' to add a child"
+        label.font = label.font.withSize(40)
+        label.backgroundColor = UIColor.white
+        label.textColor = UIColor.lightGray
+        self.view.addSubview(label)
+        
+        return label
+        
+    }()
     
     lazy var fetchedResultsController = { () -> NSFetchedResultsController<Student> in
         let request: NSFetchRequest<Student> = Student.fetchRequest()
@@ -28,7 +40,43 @@ class StudentListViewController: UITableViewController, NSFetchedResultsControll
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // NavBar
+        navBarSetup()
+
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        fetchedResultsController.delegate = self
+        
+        do {
+            try self.fetchedResultsController.performFetch()
+        } catch let error as NSError {
+            print ("Error fetching item objects \(error.localizedDescription), \(error.userInfo)")
+        }
+        
+        presentInstructionLabel()
+    }
+    
+    override func viewDidLayoutSubviews() {
+       
+        super.viewDidLayoutSubviews()
+        
+        instructionsLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            instructionsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            instructionsLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            instructionsLabel.heightAnchor.constraint(equalToConstant: 80)
+            ])
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func navBarSetup() {
+        
         settingsButton = UIBarButtonItem(title: "Settings", style: .plain, target: self, action: #selector(settingsWasPressed))
         addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addStudentPressed))
         allContentButton = UIBarButtonItem(title: "All Content", style: .plain, target: self, action: #selector(allContentPressed))
@@ -40,41 +88,7 @@ class StudentListViewController: UITableViewController, NSFetchedResultsControll
         
         let rightSpaceItem = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
         self.navigationItem.rightBarButtonItems = [allContentButton, rightSpaceItem, addButton]
-
-
-        // TableView
-        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         
-        // FetchedResultsController
-        fetchedResultsController.delegate = self
-        
-        do {
-            try self.fetchedResultsController.performFetch()
-        } catch let error as NSError {
-            print ("Error fetching item objects \(error.localizedDescription), \(error.userInfo)")
-        }
-        
-        // InstructionsLabel
-        instructionsLabel.text = "Tap '+' to add a child"
-        instructionsLabel.font = instructionsLabel.font.withSize(40)
-        instructionsLabel.backgroundColor = UIColor.white
-        instructionsLabel.textColor = UIColor.lightGray
-        self.view.addSubview(instructionsLabel)
-        
-        instructionsLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            instructionsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            instructionsLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            instructionsLabel.heightAnchor.constraint(equalToConstant: 80)
-            ])
-        
-        presentInstructionLabel()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func presentInstructionLabel() {
@@ -90,63 +104,6 @@ class StudentListViewController: UITableViewController, NSFetchedResultsControll
         }
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return fetchedResultsController.sections?.count ?? 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       
-        guard let section = fetchedResultsController.sections?[section] else {
-            return 0
-        }
-        
-        return section.numberOfObjects
-    }
-  
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-        let student = fetchedResultsController.object(at: indexPath)
-        cell.textLabel?.text = student.name
-
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        let student = fetchedResultsController.object(at: indexPath)
-        
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
-        flowLayout.itemSize = CGSize(width: 300, height: 300)
-        
-        let teacherCollectionViewController = TeacherCollectionViewController(student: student, collectionViewLayout: flowLayout)
-        
-        self.navigationController?.pushViewController(teacherCollectionViewController, animated: true)
-        
-    }
-
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        
-        return true
-    }
-
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        
-        let student = fetchedResultsController.object(at: indexPath)
-        
-        DataController.sharedInstance.managedObjectContext.delete(student)
-        DataController.sharedInstance.saveContext()
-    }
-
-
-    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.reloadData()
-        presentInstructionLabel()
-    }
-    
     // MARK: NavBar Actions
     
     func editPasswordWasPressed() {
@@ -193,4 +150,72 @@ class StudentListViewController: UITableViewController, NSFetchedResultsControll
         self.navigationController?.pushViewController(allContentViewController, animated: true)
     }
 
+}
+
+// MARK: UITableViewDataSource & Delegate
+
+extension StudentListViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        guard let section = fetchedResultsController.sections?[section] else {
+            return 0
+        }
+        
+        return section.numberOfObjects
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let student = fetchedResultsController.object(at: indexPath)
+        cell.textLabel?.text = student.name
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let student = fetchedResultsController.object(at: indexPath)
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        flowLayout.itemSize = CGSize(width: 300, height: 300)
+        
+        let teacherCollectionViewController = TeacherCollectionViewController(student: student, collectionViewLayout: flowLayout)
+        
+        self.navigationController?.pushViewController(teacherCollectionViewController, animated: true)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        let student = fetchedResultsController.object(at: indexPath)
+        
+        DataController.sharedInstance.managedObjectContext.delete(student)
+        DataController.sharedInstance.saveContext()
+    }
+
+    
+}
+
+// MARK: NSFetchedResultsControllerDelegate
+
+extension StudentListViewController: NSFetchedResultsControllerDelegate {
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        
+        tableView.reloadData()
+        presentInstructionLabel()
+        
+    }
 }
